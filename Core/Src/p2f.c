c4d9d931fbdbb6e7b6192dc6b742ef121f1695c2
Guillem Vilchez <1745991@campus.euss.org>
2026-04-20 15:23:23 +0200

    - Implementació de la apps a p2f. - Implementació de normativa a la funció.

--- Core/Src/p2f.c
+++ Core/Src/p2f.c
@@ -8,17 +8,16 @@ uint8_t 	TSoff  = DICCP->FpINTtsoff;							// Estat del tsoff (pressió OK)
 uint8_t 	Button_R2DX = DICCP->FpINTr2d; 						// Estat del botó Ready To Drive
 uint32_t 	Brake = DICCF->FfANLbrake;							// Valor que llegeix el ADC del sensor de fre
 uint8_t 	Estat_r2d = DICCP-> FpDIGr2d;						// Estat final que determinarà si el cotxe està ready to drive o no
-uint8_t 	switch_state = 0;									// Estat en el que es troba el r2d
+uint8_t 	switch_state_r = 0;									// Estat en el que es troba el r2d
 uint32_t 	temp_R2D = 0;										// Temps (en ms) en què entrem a STEP2
 
-	// Màquina d'estats finits per la seqüència Ready To Drive
-	switch (switch_state)
+	switch (switch_state_r)										// Màquina d'estats finits per la seqüència Ready To Drive
 	{
 		// Condicions per passar d'inicial a STEP1: fre premut (Brake >= 300), SDC actiu (SDC != 0), botó R2D premut (Button_R2DX != 0),
 		//Aire positiu OK (TSoff != 0) i sense error d'APPS (!error_apps)
 		case 0:
 			if (Brake >= 300 && SDC == 1 && Button_R2DX == 1 && TSoff == 1){
-				switch_state = 1;
+				switch_state_r = 1;
 			}
 			break;
 
@@ -26,40 +25,40 @@ uint32_t 	temp_R2D = 0;										// Temps (en ms) en què entrem a STEP2
 		case 1:
 			if (Brake >= 300 && SDC == 1 && Button_R2DX != 1 && TSoff == 1)
 			{
-				temp_R2D = HAL_GetTick();			// HAL_GetTick() dona el nombre de mil·lisegons des de HAL_Init() (és un contador global de temps del sistema incrementat per l'interrupt de SysTick).
-				switch_state = 2;					// Passem a STEP2 (finestra d'espera de 2 segons)
+				temp_R2D = HAL_GetTick();						// HAL_GetTick() dona el nombre de mil·lisegons des de HAL_Init() (és un contador global de temps del sistema incrementat per l'interrupt de SysTick).
+				switch_state_r = 2;								// Passem a STEP2 (finestra d'espera de 2 segons)
 			}
-			else if (SDC == 0 || TSoff == 0){ 		// Si es perd SDC, air positiu o hi ha error d'APPS, tornem a l'estat inicial
-				switch_state = 0;
+			else if (SDC == 0 || TSoff == 0){ 					// Si es perd SDC, air positiu o hi ha error d'APPS, tornem a l'estat inicial
+				switch_state_r = 0;
 			}
 			break;
 
 		// En STEP2: SDC actiu, botó segueix alliberat, air positiu OK, sense error APPS i han passat com a mínim 2000 ms (2 s) des de temp_R2D
 		case 2:
-			if (Brake >= 300 && SDC == 1 && TSoff == 1 && (HAL_GetTick() - temp_R2D) >= 2000){		// Aquí, determinem el temps que s'ha avançat respecte el punt inicial
-				switch_state = 3;																			// Si es compleix tot això, passem a STEP3
+			if (Brake >= 300 && SDC == 1 && TSoff == 1 && (HAL_GetTick() - temp_R2D) >= 2000){				// Aquí, determinem el temps que s'ha avançat respecte el punt inicial
+				switch_state_r = 3;																			// Si es compleix tot això, passem a STEP3
 			}
 			else if (SDC == 0 || TSoff == 0 || Brake <= 300)	{											// Qualsevol pèrdua de SDC, air positiu o error APPS ens fa tornar a inicial
-				switch_state = 0;
+				switch_state_r = 0;
 			}
 			break;
 
 		// En Ready To Drive, vigilem contínuament que: SDC segueixi actiu, air positiu OK i sense error d'APPS
 		case 3:
 			if (SDC == 0 || TSoff == 0){
-				switch_state = 0;
+				switch_state_r = 0;
 			}
 			break;
 	}
 
 	// Sortides segons l'estat
-	if (switch_state == 0 || switch_state == 1)
+	if (switch_state_r == 0 || switch_state_r == 1)
 	{
 		HAL_GPIO_WritePin(GPIOB, Buzzer_Pin, GPIO_PIN_RESET); 			//Estat buzzer en repòs
 		HAL_GPIO_WritePin(GPIOB, Enable_Pin, GPIO_PIN_RESET);			//Estat enable en repòs
 		Estat_r2d = 0;
 	}
-	else if (switch_state == 2)
+	else if (switch_state_r == 2)
 	{
 		HAL_GPIO_WritePin(GPIOB, Buzzer_Pin, GPIO_PIN_SET);				//Estat buzzer actiu
 		HAL_GPIO_WritePin(GPIOB, Enable_Pin, GPIO_PIN_RESET);			//Estat enable en repòs
@@ -71,4 +70,53 @@ uint32_t 	temp_R2D = 0;										// Temps (en ms) en què entrem a STEP2
 		HAL_GPIO_WritePin(GPIOB, Enable_Pin, GPIO_PIN_SET);				//Estat enable en actiu
 		Estat_r2d = 1;
 	}
+}
+
+void APPS(DICCF_t *DICCF, DICCP_t *DICCP){
+/*------------VARIABLES APPS-----------*/
+int32_t 	RPotX = DICCF -> FfANLRpot;									// Valor que llegeix el ADC del potenciometre dret de l'accelerador
+int32_t 	LPotX = DICCF -> FfANLLpot;									// Valor que llegeix el ADC del potenciometre esquerre de l'accelerador
+uint8_t 	Estat_apps = DICCP-> FpERRapps;								// Estat final que determinarà si el cotxe té una implausibilitat o no
+uint8_t 	switch_state_a = 0;											// Estat en el que es troba el apps
+uint8_t		Perc_Pright = (RPotX - Rpotmin)/((Rpotmax - Rpotmin)/100);  // Quantitat de bits que canvia el senyal del potenciometre dret per cada % que trepitjes el pedal dret.
+uint8_t		Perc_Pleft = (LPotX - Lpotmin)/((Lpotmax - Lpotmin)/100);   // Quantitat de bits que canvia el senyal del potenciometre esquerra per cada % que trepitjes el pedal esquerra.
+uint32_t 	APPS_temp=0;												// Temps (en ms) en què entrem a STEP1
+
+	switch(switch_state_a) 												// Estructura de control per gestionar en quin punt de la lògica APPS ens trobem.
+	{
+
+	  // Estat base: Tot funciona correctament.
+	  case 0:
+		  /* EXPLICACIÓ (int32_t): Forcem el canvi de tipus a enter amb signe.
+		   * Si RPotX (100) < LPotX (500), la resta directa donaria un valor positiu gegant (overflow).
+		   * Amb (int32_t), la resta dóna -400, i abs() pot convertir-ho correctament a 400.*/
+		  if( abs((int32_t)Perc_Pright - (int32_t)Perc_Pleft) >= 10){
+			  APPS_temp = HAL_GetTick(); 															// Error detectat: Guardem el "timestamp" actual en mil·lisegons.
+			  switch_state_a = 1;}   																// Passem a l'estat de verificació (comprovar si l'error dura 500ms).
+		else if( RPotX <= Rpotmin || LPotX <= Lpotmin || RPotX >= Rpotmax || LPotX >= Lpotmax){		// Comprova si els sensors estan fora de rang (per sota de 10 o per sobre de 1000).
+			  switch_state_a = 2;}   																// Error crític immediat (ex: cable tallat), anem a l'estat de falla.
+		break;
+
+	  // Estat de "confirmació" d'error de plausibilitat.
+	  case 1:
+		  if ((HAL_GetTick() - APPS_temp) >= 100){										// Si la diferència ha persistit durant 500ms o més (regla T.4.2 de Formula Student).
+			  switch_state_a = 2;}   													// L'error és real i persistent, bloquegem el sistema.
+		  else if ( abs((int32_t)Perc_Pright - (int32_t)Perc_Pleft) <= 10){				// Si la diferència torna a valors acceptables abans dels 500ms.
+			  switch_state_a = 0;} 														// Ha estat un soroll transitori, tornem a l'estat normal.
+		  break;
+
+	  // Estat d'error crític (Shutdown).
+	  case 2:
+		  switch_state_a = 2; 															// Bucle infinit en aquest estat: el cotxe no pot accelerar fins a reiniciar.
+		  break;
+	}
+	// Sortides segons l'estat
+		if (switch_state_a == 0 || switch_state_a == 1)
+		{
+			Estat_apps = 0;
+		}
+		else
+		{
+			Estat_apps = 1;
+		}
 }
